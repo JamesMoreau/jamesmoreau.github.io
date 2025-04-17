34815ce7d3f6cd7209924c6f4705ed4010be6767
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2025-04-16 21:59:26 -0400

    fix: Update WorkLink text formatting and adjust sizes for improved readability

--- lib/tabs/work.dart
+++ lib/tabs/work.dart
@@ -19,32 +19,32 @@ class WorkTab extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                 child: SizedBox(
-                  width: 700,
+                  width: 750,
                   child: ListView(
                     shrinkWrap: true,
                     children: const [
                       WorkLink(
-                        text: 'Purolator Delivery Pro - Last-mile delivery service',
+                        text: 'Purolator Delivery Pro / Last-mile delivery service',
                         url: 'https://apps.apple.com/ca/app/purolator-delivery-pro/id1622239326',
                       ),
                       WorkLink(
-                        text: 'Keyed - A simple password generator with WASM',
+                        text: 'Keyed / A simple password generator with WASM',
                         url: 'https://jamesmoreau.github.io/keyed/',
                       ),
                       WorkLink(
-                        text: 'Spot Alert - A proximity based alarm app',
+                        text: 'Spot Alert / A proximity based alarm app',
                         url: 'https://apps.apple.com/ca/app/spot-alert/id6478944468',
                       ),
                       WorkLink(
-                        text: 'GemPaint - A WASM based paint program',
+                        text: 'GemPaint / A WASM based paint program',
                         url: 'https://jamesmoreau.github.io/GemPaint/',
                       ),
                       WorkLink(
-                        text: 'Pixel8 - Convert any image to pixel art',
+                        text: 'Pixel8 / Convert any image to pixel art',
                         url: 'https://jamesmoreau.github.io/pixel8/',
                       ),
                       WorkLink(
-                        text: 'GemPlayer - A lightweight music player',
+                        text: 'GemPlayer / A lightweight music player',
                         url: 'https://github.com/JamesMoreau/gem-player',
                       ),
                     ],
@@ -109,8 +109,8 @@ class WorkLink extends StatelessWidget {
             onTap: () => launchMyUrl(url),
             child: HoverText(
               text: text,
-              bigTextSize: 40,
-              smallTextSize: 25,
+              bigTextSize: 24,
+              smallTextSize: 20,
             ),
           ),
           const SizedBox(width: 10),
